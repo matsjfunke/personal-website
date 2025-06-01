c9a081e1786323e8b7ff7c0a730cb45eeb54dcb4
Mats Julius Funke <125814808+matsjfunke@users.noreply.github.com>
2025-06-01 23:45:41 +0200

    feat: added discovery thought

--- src/data/thoughts.ts
+++ src/data/thoughts.ts
@@ -2,17 +2,17 @@ import { ThoughtMeta } from "@/types/thought";
 
 export const thoughts: ThoughtMeta[] = [
   {
-    title: "There is nothing to invent",
-    abstract: "Are solutions invented or discovered?",
-    date: "2025-06-01",
-    author: "matsjfunke",
-    slug: "discovery",
+    "title": "There's nothing to invent",
+    "abstract": "Are solutions invented or discovered?",
+    "date": "2025-06-01",
+    "author": "matsjfunke",
+    "slug": "discovery"
   },
   {
-    title: "Falling in love with the CLI",
-    abstract: "From tedious GUI life to Terminal enjoyment",
-    date: "2025-01-19",
-    author: "matsjfunke",
-    slug: "cli-love",
-  },
+    "title": "Falling in love with the CLI",
+    "abstract": "From tedious GUI life to Terminal enjoyment",
+    "date": "2025-01-19",
+    "author": "matsjfunke",
+    "slug": "cli-love"
+  }
 ];
