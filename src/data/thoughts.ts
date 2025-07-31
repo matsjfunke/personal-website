6c8dd75592efd58c0f5c3f411ffef3f15a0e3910
Mats Julius Funke <125814808+matsjfunke@users.noreply.github.com>
2025-07-31 22:46:40 +0200

    feat: added context thought

--- src/data/thoughts.ts
+++ src/data/thoughts.ts
@@ -2,31 +2,31 @@ import { ThoughtMeta } from "@/types/thought";
 
 export const thoughts: ThoughtMeta[] = [
   {
-    title: "Unlocking a new era of Expertise",
-    abstract: "Context is the missing piece for LLMs to get actual work done",
-    date: "2025-07-30",
-    author: "matsjfunke",
-    slug: "context",
+    "title": "Unlocking a new era of Agentic Expertise",
+    "abstract": "Context is the missing piece for LLM Agents to get actual work done",
+    "date": "2025-07-30",
+    "author": "matsjfunke",
+    "slug": "context"
   },
   {
-    title: "Human learning in the age of AI",
-    abstract: "Is knowledge still relevant if AI knows everything?",
-    date: "2025-06-28",
-    author: "matsjfunke",
-    slug: "knowledge-and-ai",
+    "title": "Human learning in the age of AI",
+    "abstract": "Is knowledge still relevant if AI knows everything?",
+    "date": "2025-06-28",
+    "author": "matsjfunke",
+    "slug": "knowledge-and-ai"
   },
   {
-    title: "Discovery over Invention",
-    abstract: "Are solutions invented or discovered?",
-    date: "2025-06-01",
-    author: "matsjfunke",
-    slug: "discovery",
+    "title": "Discovery over Invention",
+    "abstract": "Are solutions invented or discovered?",
+    "date": "2025-06-01",
+    "author": "matsjfunke",
+    "slug": "discovery"
   },
   {
-    title: "Falling in love with the CLI",
-    abstract: "From tedious GUI life to Terminal enjoyment",
-    date: "2025-01-19",
-    author: "matsjfunke",
-    slug: "cli-love",
-  },
+    "title": "Falling in love with the CLI",
+    "abstract": "From tedious GUI life to Terminal enjoyment",
+    "date": "2025-01-19",
+    "author": "matsjfunke",
+    "slug": "cli-love"
+  }
 ];
